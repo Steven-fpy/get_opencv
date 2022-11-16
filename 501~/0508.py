bac93b56c30c7d0bae62cfa13d22d0e4d958a8d6
Steven-fpy <steven8175@gmail.com>
2022-11-16 13:16:23 +0900

    update

--- 501~/0508.py
+++ 501~/0508.py
@@ -16,6 +16,16 @@ hist = cv2.calcHist([roi_h], [0], None, [64], [0,256])
 backP = cv2.calcBackProject([h.astype(np.float32)], [0],
                             hist, [0, 256], scale = 1.0)
 
-
+#minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(backP)
+#T = maxVal - 1     #threshold
 
 #3
+hist = cv2.sort(hist, cv2.SORT_EVERY_COLUMN+cv2.SORT_DESCENDING)
+k = 1
+T = hist[k][0] - 1      #threshold
+print('T =', T)
+ret, dst = cv2.threshold(backP, T, 255, cv2.THRESH_BINARY)
+
+cv2.imshow('dst', dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
