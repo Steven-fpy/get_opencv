b0574375e67878a9eb537d0dbfc19727bb889054
Steven-fpy <steven8175@gmail.com>
2022-12-02 13:12:44 +0900

    update_files and README_fix

--- Clone/application.py
+++ Clone/application.py
@@ -8,3 +8,10 @@ def hello():
 
 if __name__ == "__main__":
     application.run(host = '0.0.0.0')
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
