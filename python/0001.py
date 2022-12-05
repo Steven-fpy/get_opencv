5b0438562ca3e4a95638ac7a9171c340b5f26e9e
Steven-fpy <steven8175@gmail.com>
2022-12-05 17:59:06 +0900

    H_bridge update

--- python/0001.py
+++ python/0001.py
@@ -1,8 +1,16 @@
-# 0001.py   221202
-# t = (1,2),[3,4],5,
-# print(type(t))
-# print(t)/
-# print(t[0])
+# # 0001.py   221202
+# # t = (1,2),[3,4],5,
+# # print(type(t))
+# # print(t)/
+# # print(t[0])
 
-o = 1,2,3
-print(o) 
+# o = 1,2,3
+# print(o) 
+
+import math
+import matplotlib as mp
+import cv2
+import numpy as np
+
+a,b = input('밑변과 높이를 입력하세요(쉼표로 구분) : ')
+print(a,b)
